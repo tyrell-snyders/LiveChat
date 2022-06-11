7b7647d8a2944155d9e189afde92ad0a1f87a769
SFSADespair <tyrell.snyders@gmail.com>
2022-06-11 10:59:39 +0200

    Added new post request for login in userRoutes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,8 @@
-const { register } = require('../controllers/userController')
+const { register, login } = require('../controllers/userController')
 
 const router = require('express').Router()
 
 router.post('/register', register) //posting the data retrieved from the register to the database
+router.post('/login', login) //posting the data retrieved from the login to the database
 
 module.exports = router
