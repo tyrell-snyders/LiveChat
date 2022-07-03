da2c59dcb89b3a783d6731cc1c416c94ebef50eb
SFSADespair <tyrell.snyders@gmail.com>
2022-07-03 19:11:21 +0200

    Added a feature messagesController to send the message tio the database

--- server/controllers/messagesController.js
+++ server/controllers/messagesController.js
@@ -1,5 +1,17 @@
 module.exports.addMsg = async(req, res, next) => {
+    try {
+        const { from, to, message } = req.body
+        const data = await messageModel.create({
+            message: { text: message },
+            users: [from, to],
+            sender: from
+        })
+        if (data) return res.json({ msg: "Message send successfully." })
+        else return res.json({ msg: "Failed to send message" })
+    } catch (ex) {
+        next(ex)
 
+    }
 }
 
 module.exports.getAllMsg = async(req, res, next) => {
